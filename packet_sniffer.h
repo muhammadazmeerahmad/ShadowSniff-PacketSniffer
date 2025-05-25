01c2ee6beedc8eb7ab2fed06551ccae7a5d2be8e
Muhammad Azmeer Ahmad <azmeerbaloch29@gmail.com>
2025-05-25 20:21:20 +0500

    Delete packet_sniffer.h

--- packet_sniffer.h
+++
@@ -1,8 +0,0 @@
-#ifndef PACKET_SNIFFER_H   // Include guard to prevent multiple inclusion
-#define PACKET_SNIFFER_H
-
-// Function prototype for packet sniffing logic
-void start_sniffing();
-
-#endif
-
