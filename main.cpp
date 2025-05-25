27acc92860439303d5d352ac59c63fcca469073a
Muhammad Azmeer Ahmad <azmeerbaloch29@gmail.com>
2025-05-25 20:22:51 +0500

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "packet_sniffer.h"  // Include your packet sniffer header
-int main() {
-    printf("?? ShadowSniff-C Packet Analyzer Starting...\n");
-    // Start the packet sniffer (defined in packet_sniffer.c)
-
-   	start_sniffing();
-}
-
